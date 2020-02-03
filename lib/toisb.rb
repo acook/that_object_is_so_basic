f80960dc672376717ff18e84239107c200b8dd7e
Anthony M. Cook <github@anthonymcook.com>
2020-02-02 23:19:39 -0600

    Adding trace optional parameter to TOISB.wrap
    This allows the programmer to easily override the `@trace` instance variable to ideally make debugging easier when metaprogramming.

--- lib/toisb.rb
+++ lib/toisb.rb
@@ -1,7 +1,7 @@
 require_relative "toisb/wrapper"
 
 module TOISB
-  def self.wrap object
-    Wrapper.new object, caller
+  def self.wrap object, trace = nil
+    Wrapper.new object, (trace || caller)
   end
 end
